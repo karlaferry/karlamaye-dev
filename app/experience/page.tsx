e08573c89de21e103ecfeea54b7451afeff7c34e
karlaferry <karlacarreon.1994@gmail.com>
2024-04-23 20:51:01 +0100

    add skill badges

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,8 +1,40 @@
 import React from "react";
 
 export default function Portfolio() {
+  const salecycleSkills = [
+    "TypeScript",
+    "Azure Data Studio",
+    "HandlebarJS",
+    "AWS",
+    "Bash",
+  ];
+
+  const mentorSkills = [
+    "NextJS",
+    "GatsbyJS",
+    "GraphQL",
+    "Basic C++",
+    "Basic Python",
+  ];
+
+  const traineeSkills = [
+    "Git",
+    "GitHub",
+    "JavaScript",
+    "HTML",
+    "CSS",
+    "ReactJS",
+    "React Native",
+    "ExpressJS",
+    "NodeJS",
+    "Tailwind CSS",
+    "MySQL",
+    "PostgreSQL",
+    "MongoDB",
+    "Firebase",
+  ];
   return (
-    <div className="opacity-0 animate-fade-in">
+    <div className="opacity-0 animate-fade-in p-6 py-6">
       <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
         <li>
           <div className="timeline-middle">
@@ -21,14 +53,23 @@ export default function Portfolio() {
           </div>
           <div className="timeline-start md:text-end mb-10">
             <time className="font-mono italic">Aug 2022 - Present</time>
-            <div className="text-lg font-black">
+            <div className="text-lg font-bold font-mono">
               SaleCycle | Junior Developer
             </div>
-            Develops bespoke script implementations for onboarding clients,
-            designed to function as web scrapers on their sites which enable the
-            seamless execution of abandonment campaigns, facilitating targeted
-            engagement strategies to enhance client retention and conversion
-            rates.
+            <div className="text-xs pb-2">
+              Develops bespoke script implementations for onboarding clients,
+              designed to function as web scrapers on their sites which enable
+              the seamless execution of abandonment campaigns, facilitating
+              targeted engagement strategies to enhance client retention and
+              conversion rates.
+            </div>
+            <div className="my-2">
+              {salecycleSkills.map((skill) => (
+                <span className="badge badge-outline text-xs mx-1 my-2">
+                  {skill}
+                </span>
+              ))}
+            </div>
           </div>
           <hr />
         </li>
@@ -52,18 +93,27 @@ export default function Portfolio() {
             <time className="font-mono italic">
               February 2022 – August 2022
             </time>
-            <div className="text-lg font-black">
+            <div className="text-lg font-bold font-mono">
               Northcoders | Junior Software Engineer & Mentor
             </div>
-            Delivered comprehensive mentoring and code support to bootcamp
-            students and candidates, offering expert guidance in
-            troubleshooting, debugging, and conducting thorough code reviews.
-            <br />
-            <br />
-            Provided pastoral support and fostered a supportive learning
-            environment. Actively engaged in internal development initiatives,
-            facilitating exploration of new technologies and fostering personal
-            growth opportunities.
+            <div className="text-xs pb-2">
+              Delivered comprehensive mentoring and code support to bootcamp
+              students and candidates, offering expert guidance in
+              troubleshooting, debugging, and conducting thorough code reviews.
+            </div>
+            <div className="text-xs pb-2">
+              Provided pastoral support and fostered a supportive learning
+              environment. Actively engaged in internal development initiatives,
+              facilitating exploration of new technologies and fostering
+              personal growth opportunities.
+            </div>
+            <div className="my-2">
+              {mentorSkills.map((skill) => (
+                <span className="badge badge-outline text-xs mx-1">
+                  {skill}
+                </span>
+              ))}
+            </div>
           </div>
           <hr />
         </li>
@@ -87,15 +137,26 @@ export default function Portfolio() {
             <time className="font-mono italic">
               October 2021 – February 2022
             </time>
-            <div className="text-lg font-black">
+            <div className="text-lg font-bold font-mono">
               Northcoders | Trainee Software Developer
             </div>
-            Successfully completed an intensive 13-week full-stack development
-            bootcamp within a dynamic and collaborative environment. Emphasized
-            industry-leading practices, including the implementation of semantic
-            and clean code, Continuous Integration/Continuous Deployment
-            (CI/CD), Test-Driven Development (TDD), and honed soft skills
-            crucial for professional growth.
+            <div className="text-xs pb-2">
+              Successfully completed an intensive 13-week full-stack development
+              bootcamp within a dynamic and collaborative environment.
+            </div>
+            <div className="text-xs pb-2">
+              Emphasized industry-leading practices, including the
+              implementation of semantic and clean code, Continuous
+              Integration/Continuous Deployment (CI/CD), Test-Driven Development
+              (TDD), and honed soft skills crucial for professional growth.
+            </div>
+            <div className="my-2">
+              {traineeSkills.map((skill) => (
+                <span className="badge badge-outline text-xs mx-1 my-1">
+                  {skill}
+                </span>
+              ))}
+            </div>
           </div>
           <hr />
         </li>
