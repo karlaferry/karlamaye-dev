aaace91d16ce34b44937561eb2fdaa6fcc4e5569
Karla Ferry <karlacarreon.1994@gmail.com>
2024-04-24 12:48:13 +0100

    add thank you page

--- app/contact/thankyou/page.tsx
+++ app/contact/thankyou/page.tsx
@@ -1,5 +1,18 @@
+import Link from "next/link";
 import React from "react";
+import { IoMdArrowBack } from "react-icons/io";
 
 export default function ThankYou() {
-  return <div>ThankYou</div>;
+  return (
+    <div className="opacity-0 animate-fade-in md:m-20 m-10 flex justify-around flex-col items-center">
+      <h2 className="text-lg m-5">
+        Thank you! I will respond as soon as I can.
+      </h2>
+      <Link href="/contact">
+        <button className="btn btn-primary hover:btn-accent">
+          <IoMdArrowBack size={20} /> back
+        </button>
+      </Link>
+    </div>
+  );
 }
